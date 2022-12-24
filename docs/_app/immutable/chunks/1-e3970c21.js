6c300002b4d2c60907c8f1863bd9eeac49a5715a
David Callanan <40340425+davidcallanan@users.noreply.github.com>
2022-12-24 02:39:25 +0000

    update

--- docs/_app/immutable/chunks/1-e3970c21.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-77afa6bd.js";export{t as component};
---
+++ docs/_app/immutable/chunks/4-4a6e58c2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/quotes/_page.svelte-6983a8c6.js";export{t as component};
